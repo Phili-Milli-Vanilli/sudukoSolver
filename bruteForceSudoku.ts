d5d87833e50a6a60b5536e08e62b4849aa0b13f0
Phil Thorenz <phil.thorenz@telekom.de>
2023-09-20 14:30:03 +0200

    Hardcode function

--- bruteForceSudoku.ts
+++ bruteForceSudoku.ts
@@ -1,6 +1,3 @@
-import { isValid, printSudoku } from "./backTrackingSudoku";
-
-
 function isSudokuSolved(sudoku: number[][]) {
     for (let row = 0; row < 9; row++) {
         for (let col = 0; col < 9; col++) {
@@ -15,11 +12,60 @@ function isSudokuSolved(sudoku: number[][]) {
     return true;  // Sudoku ist gelöst
 }
 
+export const isValid = (sudukoTable: number[][], row: number, col: number, num: number) => {
+    for(let i = 0; i < 9; i++){
+        if(sudukoTable[row][i] === num){
+            return false;
+        }
+    }
+
+    for(let i = 0; i < 9; i++){
+        if(sudukoTable[i][col] === num){
+            return false;
+        }
+    }
+
+    const startRow = Math.floor(row / 3) * 3;
+    const startCol = Math.floor(col / 3) * 3;
+    for(let i = startRow; i < startRow + 3; i++){
+        for(let j = startCol; j < startCol + 3; j++){
+            if(sudukoTable[i][j] === num){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+export function printSudoku(sudoku: number[][] | undefined | true) {
+    if(sudoku === undefined || sudoku === true){
+        console.log('kein printbares Array')
+        console.log(sudoku)
+        return
+    }
+    for (let i = 0; i < 9; i++) {
+        if (i % 3 === 0 && i !== 0) {
+            console.log("- - - - - - - - - - -");
+        }
+        for (let j = 0; j < 9; j++) {
+            if (j % 3 === 0 && j !== 0) {
+                process.stdout.write("| ");
+            }
+            if (j === 8) {
+                console.log(sudoku[i][j]);
+            } else {
+                process.stdout.write(sudoku[i][j] === 0 ? "  " : sudoku[i][j] + " ");
+            }
+        }
+    }
+}
+
 function bruteForceSudoku(sudoku: number[][], row = 0, col = 0) {
     if (row === 9) {
         if (isSudokuSolved(sudoku)) {
             return true;  // Sudoku ist gelöst
         }
+        console.log('1')
         return false;  // Sudoku ist nicht gelöst
     }
 
@@ -55,6 +101,18 @@ const unsolvedSudoku = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
+
+const unsolvedSudoku2 = [
+    [1, 6, 3, 8, 2, 5, 7, 0, 9],
+    [9, 5, 8, 6, 7, 4, 0, 1, 3],
+    [2, 0, 4, 3, 1, 9, 8, 5, 6],
+    [4, 1, 7, 5, 0, 8, 0, 9, 2],
+    [0, 8, 5, 2, 9, 3, 4, 7, 1],
+    [3, 9, 2, 0, 7, 0, 5, 6, 8],
+    [5, 0, 6, 9, 8, 7, 1, 3, 2],
+    [8, 3, 1, 7, 4, 2, 9, 6, 5],
+    [7, 2, 9, 1, 3, 6, 0, 8, 4]
+]
 
 bruteForceSudoku(unsolvedSudoku);
 
